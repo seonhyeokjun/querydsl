6d8adf6e21f13bcdecc53e97c7ea1ef020f81577
seonhyeokjun <tjsrmatn@naver.com>
2022-05-28 23:18:20 +0900

    Initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,17 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
