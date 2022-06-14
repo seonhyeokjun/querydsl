7b88d5909b3fd470a5f83abb6bd8f752753b5433
seonhyeokjun <tjsrmatn@naver.com>
2022-06-15 00:45:18 +0900

    예제 도메인 모델

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -2,7 +2,9 @@ package study.querydsl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
